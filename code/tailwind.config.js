09ada3ce983ff5c1b5bbf25d9f9e8350844d40bb
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-04 20:44:18 +0900

    Install tailwindcss

---
+++ code/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+
+
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b", // black?
+        light: "#f5f5f5", // white
+        primary: "#B63E96", // dark pink
+        primaryDark: "#58E6D9", //electric blue
+      },
+    },
+  },
+  plugins: [],
+}
+
