1029243f5ae800583fd0ddf8b55c2b723f946e8b
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-07 21:53:06 +0900

    Create AnimatedText component; style homepage hero text

--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -2,6 +2,7 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/greyshirt.png";
+import AnimatedText from '@/components/AnimatedText';
 
 export default function Home() {
   return (
@@ -17,13 +18,15 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={profilePic} alt="CharlieJ" className='w-full h-auto ' />
             </div>
-            <div className='w-1/2'>
-              <h1>Turning Vision Into Reality With Code And Design.
-                <p>
-                  As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
-                  Explore my latest projects and articles, showcasing my expertise in React.js and web development.
-                </p>
-              </h1>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-5xl !text-left' />
+
+              <p className='my=4 text-base font-medium'>
+                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+              </p>
+
             </div>
           </div>
         </Layout>
