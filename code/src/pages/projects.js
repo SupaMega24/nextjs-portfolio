62a4e5b5ac74cb39ce63237d7f8a1ac1a62d7fbb
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-12 18:01:10 +0900

    Create FeaturedProject component

---
+++ code/src/pages/projects.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GithubIcon } from '@/components/Icons'
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+    return (
+        <article className='w-full flex items-center justify-between
+            rounded-3xl border border-solid border-dark bg-light
+            shadow-2xl p-8'
+        >
+
+            <Link href={link} target="_blank" className='w-1/2 cursor-pointer
+                overflow-hidden rounded-lg'
+            >
+                <Image src={img} alt={title} className='w-full h-auto' />
+            </Link>
+
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+                <span className='text-blue font-medium text-xl '>
+                    {type}
+                </span>
+                <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-2xl font-bold'>
+                        {title}
+                    </h2>
+                </Link>
+                <p className='my-2 font-medium text-dark'>{summary}</p>
+                <div className='mt-2 flex items-center'>
+                    <Link href={github} target="_blank" className='w-14'> <GithubIcon /> </Link>
+                    <Link href={link} target="_blank" className='ml-6 rounded-lg bg-dark text-light
+                        p-2 px-6 text-lg font-semibold'
+                    >
+                        Visit Project
+                    </Link>
+                </div>
+            </div>
+
+        </article>
+    )
+}
+
+const projects = () => {
+    return (
+        <>
+
+            <Head>
+                <title>CharlieJ | Projects Page</title>
+                <meta name="description" content="any description" />
+            </Head>
+            <main className='w-full mb-16 flex flex-col items-center justify-center'>
+                <Layout className='pt-16'>
+                    <AnimatedText text="Imagination Builds Knowledge!" className='!text-6xl mb-16' />
+
+                    <div className='grid grid-cols-12 gap-24'>
+                        <div className='col-span-12'>
+                            <FeaturedProject
+
+
+                                title="Crypto Screener Application"
+                                img={project1}
+                                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                                link="/"
+                                github="/"
+                                type="Featured Project"
+                            />
+                        </div>
+
+                        <div className='col-span-6'>
+                            Project 1
+                        </div>
+
+                        <div className='col-span-6'>
+                            Project 2
+                        </div>
+                    </div>
+
+                </Layout>
+            </main>
+
+        </>
+    )
+}
+
+export default projects
