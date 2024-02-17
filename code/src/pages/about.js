4bdf0065bf593beac2e082ab414f63d30beacb28
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-02-18 01:28:52 +0900

    Create transition effect component; place it on all pages

--- code/src/pages/about.js
+++ code/src/pages/about.js
@@ -7,6 +7,7 @@ import profilePic from "../../public/images/profile/profilepic.jpeg"
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Education from '@/components/Education'
+import TransitionEffect from '@/components/TransitionEffect'
 
 const about = () => {
     return (
@@ -15,6 +16,9 @@ const about = () => {
                 <title>CharlieJ | About Page</title>
                 <meta name="description" content="any description" />
             </Head>
+
+            <TransitionEffect />
+
             <main className='flex w-full flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
                     <AnimatedText text="Passion Ignites Innovation!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl
--- code/src/pages/projects.js
+++ code/src/pages/projects.js
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { GithubIcon } from '@/components/Icons'
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import TransitionEffect from '@/components/TransitionEffect'
 
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
@@ -127,6 +128,9 @@ const projects = () => {
                 <title>CharlieJ | Projects Page</title>
                 <meta name="description" content="any description" />
             </Head>
+
+            <TransitionEffect />
+
             <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
                     <AnimatedText text="Imagination Builds Knowledge!" className='mb-16
